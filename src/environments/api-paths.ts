cf9269fd652753341099c9d20dfd571f48ccf6cc
DESKTOP-5H8EBD7\Asad <AsadXero@hotmail.com>
2022-07-21 12:10:11 +0500

    github pages test

--- src/environments/api-paths.ts
+++ src/environments/api-paths.ts
@@ -1,17 +1,17 @@
-import { environment } from "./environment";
+import { environment } from './environment';
 export const API_PATHS = {
   //Auth
-  signin: `${environment.baseUrl}/auth/signin`,
-  signup: `${environment.baseUrl}/auth/signup`,
+  signin: `${environment.baseUrl}/api/auth/signin`,
+  signup: `${environment.baseUrl}/api/auth/signup`,
 
   //Tasks
-  createNewTask: `${environment.baseUrl}/tasks`,
-  getTaskById: (id: string) => `${environment.baseUrl}/tasks/${id}`,
-  deleteTaskById: (id: string) => `${environment.baseUrl}/tasks/${id}`,
+  createNewTask: `${environment.baseUrl}/api/tasks`,
+  getTaskById: (id: string) => `${environment.baseUrl}/api/tasks/${id}`,
+  deleteTaskById: (id: string) => `${environment.baseUrl}/api/tasks/${id}`,
   updateTasksStatusById: (id: string) =>
-    `${environment.baseUrl}/tasks/${id}/status`,
+    `${environment.baseUrl}/api/tasks/${id}/status`,
   getAllTasks: (search?: string, status?: string) => {
-    let api = `${environment.baseUrl}/tasks`;
+    let api = `${environment.baseUrl}/api/tasks`;
     let first = false;
     if (search || status) {
       api += `?`;
